ac191c41a15be2d5dc65147a68d06b688be1322a
Dogukan Soysal <31596218+dogukansoysal@users.noreply.github.com>
2020-08-27 17:54:34 +0300

    Iteration 2 - Revision 1
    - Movement speed decreased 10%
    - Breakable Jumping Pads imported

--- Assets/Scripts/JumpingPad.cs
+++ Assets/Scripts/JumpingPad.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class JumpingPad : MonoBehaviour
 {
-    public int Health;
+    public int Health = 4;
     public float BounceMagnitude;
     public int Index;
-    
+
+    public List<GameObject> BreakableModels;
+
     /// <summary>
     /// Generic bounce method for multiple purpose.
     /// </summary>
@@ -21,18 +23,53 @@ public class JumpingPad : MonoBehaviour
             ForceMode.Impulse);
     }
 
+    public void HandleHealth()
+    {
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        if (Health - 1 < BreakableModels.Count)
+        {
+            if (transform.childCount > 2)
+            {
+                Destroy(transform.GetChild(2).gameObject);
+            }
+            var newModel = Instantiate(BreakableModels[Health - 1], transform);
+            newModel.transform.localScale = new Vector3(0.01f,0.1f,0.01f);
+            newModel.transform.localPosition = Vector3.zero + new Vector3(0,0,-1f);
+            
+            newModel.GetComponent<MeshRenderer>().sharedMaterial = transform.GetComponent<MeshRenderer>().material;
+            transform.GetComponent<MeshRenderer>().enabled = false;
+            transform.GetComponent<MeshFilter>().mesh = null;
+        }
+    }
     public void OnTriggerEnter(Collider collider)
     {
         if (collider.transform.CompareTag("Player"))
         {
+            if (GameManager.Instance.GameState == GameConstants.GameState.Playable) // Check if game is playable
+            {
+                Health--;
+            }
+                
             Bounce(collider);
             collider.transform.GetComponent<PlayerController>().HandleBounce();
         }
         else if (collider.transform.CompareTag("Enemy"))
         {
+            if(GameManager.Instance.GameState == GameConstants.GameState.Playable)    // Check if game is playable
+            {
+                Health--;
+            }   
+                
             Bounce(collider);
             collider.transform.GetComponent<EnemyAI>().HandleBounce();
 
         }
+
+        HandleHealth();
     }
 }
